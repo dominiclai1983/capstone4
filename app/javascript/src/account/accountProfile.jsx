cd8a039f79fa47f345a353f224fec43d8ae04786
dominiclai1983 <dominiclai1983@gmail.com>
2022-10-01 12:13:22 +0200

    completed account change password section

--- app/javascript/src/account/accountProfile.jsx
+++ app/javascript/src/account/accountProfile.jsx
@@ -1,13 +1,26 @@
 import axios from 'axios';
+import { set } from 'lodash';
 import React, { useState, useEffect } from 'react';
-import { Header, Table, Button, Container } from 'semantic-ui-react';
+import {
+	Header,
+	Grid,
+	Container,
+	Button,
+	Form,
+	Message,
+} from 'semantic-ui-react';
 
 const AccountProfile = () => {
 	const [username, setUsername] = useState('');
 	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
 	const [newPassword, setNewPassword] = useState('');
 	const [newPasswordConfirm, setNewPasswordConfirm] = useState('');
-	const [edit, setEdit] = useState(false);
+	const [displayEdit, setDisplayEdit] = useState(false);
+	const [displayMessage, setDisplayMessage] = useState(false);
+	const [systemMessage, setSystemMessage] = useState('');
+	const [positive, setPositive] = useState(false);
+	const [negative, setNegative] = useState(false);
 
 	useEffect(() => {
 		const fetchData = async () => {
@@ -22,28 +35,147 @@ const AccountProfile = () => {
 		fetchData();
 	}, []);
 
+	const handleSubmit = async () => {
+		const sendNewPassword = {
+			user: {
+				password: password,
+				new_password: newPassword,
+			},
+		};
+
+		setDisplayMessage(false);
+
+		if (newPassword !== newPasswordConfirm) {
+			setSystemMessage(
+				'The New Password Does Not Match With Confirm New Password'
+			);
+			setNegative(true);
+			setDisplayMessage(true);
+			return;
+		}
+
+		try {
+			const result = await axios.put('/api/users', sendNewPassword);
+			if (result.data.success) {
+				setDisplayEdit(!displayEdit);
+				setDisplayMessage(!displayMessage);
+				setPositive(true);
+				setNegative(false);
+				setSystemMessage('The New Password Has Been Set!');
+				setPassword('');
+				setNewPassword('');
+				setNewPasswordConfirm('');
+			}
+		} catch (err) {
+			console.error(err);
+		}
+	};
+
 	return (
 		<>
 			{' '}
-			<Container textAlign='center' style={{ marginTop: 60 }}>
+			<Container textAlign='center' style={{ marginTop: '60px' }}>
 				<Header as='h1'>Account Profile</Header>
-				<Table.Body>
-					<Table.Row>
-						<Table.Cell>Username</Table.Cell>
-					</Table.Row>
-					<Table.Row>
-						<Table.Cell>Email</Table.Cell>
-					</Table.Row>
-					<Table.Row>
-						<Table.Cell>
-							Password{' '}
-							<Button floated='right' labelPosition='left' primary size='small'>
-								Edit
-							</Button>
-						</Table.Cell>
-					</Table.Row>
-				</Table.Body>
+				<Grid columns='one' textAlign='left' celled>
+					<Grid.Row>
+						<Grid.Column width={12}>
+							User Name: <br />${username}
+						</Grid.Column>
+						<Grid.Column width={4}></Grid.Column>
+					</Grid.Row>
+
+					<Grid.Row>
+						<Grid.Column width={12}>
+							Email: <br />${email}
+						</Grid.Column>
+						<Grid.Column width={4}></Grid.Column>
+					</Grid.Row>
+
+					<Grid.Row>
+						<Grid.Column width={12}>
+							Password: <br />
+							•••••••
+						</Grid.Column>
+						<Grid.Column textAlign='center' width={4}>
+							{displayEdit ? null : (
+								<Button
+									float='right'
+									onClick={() => {
+										setDisplayMessage(false);
+										setDisplayEdit(!displayEdit);
+									}}
+								>
+									Edit
+								</Button>
+							)}
+						</Grid.Column>
+					</Grid.Row>
+				</Grid>
 			</Container>
+			{displayEdit && (
+				<Container textAlign='left'>
+					<Form
+						onSubmit={(e) => {
+							e.preventDefault();
+							handleSubmit();
+						}}
+					>
+						<Header as='h3'>Change Your Password!</Header>
+						<Form.Field>
+							<label>Old Password</label>
+							<input
+								type='password'
+								value={password}
+								onChange={(e) => {
+									e.preventDefault();
+									setPassword(e.target.value);
+								}}
+							/>
+						</Form.Field>
+						<Form.Field>
+							<label>New Password</label>
+							<input
+								type='password'
+								value={newPassword}
+								onChange={(e) => {
+									e.preventDefault();
+									setNewPassword(e.target.value);
+								}}
+							/>
+						</Form.Field>
+						<Form.Field>
+							<label>Confirm New Password</label>
+							<input
+								type='password'
+								value={newPasswordConfirm}
+								onChange={(e) => {
+									e.preventDefault();
+									setNewPasswordConfirm(e.target.value);
+								}}
+							/>
+						</Form.Field>
+						<Button
+							type='submit'
+							onClick={() => {
+								setDisplayEdit(!displayEdit);
+								setDisplayMessage(false);
+							}}
+						>
+							Cancel
+						</Button>
+						<Button type='submit' floated='right' color='yellow'>
+							Submit
+						</Button>
+					</Form>
+				</Container>
+			)}
+			{displayMessage && (
+				<Container textAlign='left' style={{ marginTop: '10px' }}>
+					<Message positive={positive} negative={negative}>
+						{systemMessage}
+					</Message>
+				</Container>
+			)}
 		</>
 	);
 };
